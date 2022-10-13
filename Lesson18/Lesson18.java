ccf49f7351d01499da728e02bdb801967d41d31a
v.doichev <siavaismail@gmail.com>
2022-10-13 09:26:01 +0300

    Add Lesson18 task 2

--- Lesson18/Lesson18.java
+++ Lesson18/Lesson18.java
@@ -18,12 +18,36 @@ public class Lesson18 {
         }
         while (nextIteration);
     }
+
+    public static void showArray(String[][] arr){
+        StringBuilder str = new StringBuilder("{ ");
+
+        for (int i = 0; i<arr.length; i++){
+            str.append("{");
+            for (int j = 0; j<arr[i].length; j++){
+                str.append(arr[i][j]);
+                if (arr[i].length-j>1)
+                    str.append(",");
+                else
+                    str.append("}");
+            }
+            if (arr.length-i>1)
+                str.append(",");
+            else
+                str.append(" }");
+        }
+        System.out.println();
+        System.out.println(str);
+    }
     public static void main(String[] args) {
         int[] a1 = new int[]{1,-5, 7, 0, 4, -34};
         sortirovka(a1);
         for (int i = 0; i < a1.length; i++){
             System.out.print(a1[i]+" ");
         }
+
+        String[][] array = new String[][]{{"00","01"},{"10"},{"10","11"}};
+        showArray(array);
 
     }
 }
